2d72d27422f7e56847255232ae95bb88367d9d30
Michael Adlhoch <mps.adlhoch@gmail.com>
2018-06-26 20:02:04 +0200

    added comments

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import itertools
 
 all_observations = []
-sender_observations = []
 HS = []
 
+
+# Öffnet die Textdatei, liest Zeile für Zeile ein und schließt die Datei
 def read_data():
     data_file = open("observation_mix.txt", "r")
     for line in data_file:
+        # entfernt den Doppelpunkt und erstellt ein sender_set/receiver_set
+        # diese werden als eine Observation in die all_observations Liste gespeichert
         removed_colon = line.replace(":", "").replace("  ", " ")
         value1, value2, value3, value4, value5, value6, value7, value8, value9 = removed_colon.split(" ")
         value1 = int(value1)
@@ -24,16 +27,18 @@ def read_data():
     data_file.close()
 
 
+# speichert die Empfängermengen des übergebenen Senders in sender_observations
 def return_observations_with_specific_sender(sender):
+    sender_observations = []
     for observation in all_observations:
         if sender in observation[0]:
             sender_observations.append(observation[1])
+    return sender_observations
 
 
+# berechnet alle Minimal Hitting-Sets der Größe <= m in KB
 def exact_hs(KB, m, C):
     if not KB:
-        print("hello")
-        C.sort()
         HS.append(C)
     elif m >= 1:
         B = KB[0]
@@ -42,9 +47,9 @@ def exact_hs(KB, m, C):
             r = B[index_of_observation]
             exact_hs(get_subset_of_observations_without_r(KB, r), m - 1, C + [r])
             index_of_observation += 1
-            print(index_of_observation)
 
 
+# gibt eine Liste zurück, die alle Beobachtungen aus dem superset enthält, die r nicht enthalten
 def get_subset_of_observations_without_r(superset, r):
     subset = []
     for observation in superset:
@@ -53,22 +58,35 @@ def get_subset_of_observations_without_r(superset, r):
     return subset
 
 
+# entfernt Duplikate
 def remove_duplicates_from_mhs(minimal_hitting_sets):
     return list(minimal_hitting_sets for minimal_hitting_sets, _ in itertools.groupby(minimal_hitting_sets))
 
 
+# sortiert Listen in globaler Variable HS
+def sort_mhs():
+    for mhs in HS:
+        mhs.sort()
+
+
+# Funktionsaufruf zum Starten des ExactHS Algorithmuses
 def start_hs_attack():
+    # HS global zum abspeichern definieren
     global HS
-    KB = sender_observations
-    m = 1
-    while not HS:
-        exact_hs(KB, m, [])
-        m = m + 1
-    HS = remove_duplicates_from_mhs(HS)
-    print(HS)
+    # iteriert über Sender und gibt alle entsprechenden Sendermengen in den exact_hs Algorithmus
+    # falls exact_hs terminiert wird m um eins erhöht und so lange wiederholt, bis HS nicht mehr leer ist
+    for i in range(1, 11):
+        HS = []
+        KB = return_observations_with_specific_sender(i)
+        m = 1
+        while not HS:
+            exact_hs(KB, m, [])
+            m = m + 1
+        sort_mhs()
+        HS = remove_duplicates_from_mhs(HS)
+        print("Minimal Hitting Set for Sender " + str(i) + " (m = " + str(m-1) + ")" + ": " + str(HS))
 
-
+# Funktionsaufrufe
 read_data()
-return_observations_with_specific_sender(10)
 start_hs_attack()
 
